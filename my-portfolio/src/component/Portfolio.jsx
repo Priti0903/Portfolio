0c037d1651e9232936cc619348a7f54d8b79c119
Priti <pritiv.0903@gmail.com>
2025-09-27 21:52:18 +0530

    Update Portfolio.jsx

--- my-portfolio/src/component/Portfolio.jsx
+++ my-portfolio/src/component/Portfolio.jsx
@@ -580,10 +580,12 @@ function ContactSection() {
     };
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
+      const res = await fetch("/api/contact", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(data),
+});
+
       });
 
       const result = await res.json();
